f446447932566250b6c3a4997f5eafc3c4705f0c
Operations Research Engineering Software+ <alex@oresoftware.com>
2018-11-14 18:58:32 -0800

    ores/auto-commit => 'set'

--- src/index.ts
+++ src/index.ts
@@ -12,21 +12,37 @@ export const r2gSmokeTest = function () {
 
 export interface RouteInfo {
   path: string,
+  queryParams?: {
+    [key: string]: string
+  },
+  parsedQueryParams?: {
+    [key:string]: any
+  },
   response:{
-    success: {
-    
-    },
-    error: {
-    
+    success: any,
+    error: any
+  },
+  example:{
+    response:{
+      success: any,
+      error: any
     }
   }
 }
 
+export interface RouteMap {
+  [key:string]: RouteInfo
+}
 
-export interface Entity {
-  name: string,
-  routes: {
-    [key:string]: RouteInfo
+
+export class Entity {
+  
+  name: string;
+  routes: RouteMap;
+  
+  constructor(name: string, routes?: RouteMap){
+    this.name = name;
+    this.routes = routes || {};
   }
 }
 
@@ -35,9 +51,13 @@ export interface Info {
     [key:string]: RouteInfo
   },
   entities: {
-  
+    [key: string]: Entity
   }
 }
+
+export const joinMessages = (...args: string[]) => {
+  return args.join(' ');
+};
 
 
 export class DocGen {
@@ -52,19 +72,28 @@ export class DocGen {
     };
   }
   
-  createEntity(name: string): Entity{
-    return {
+  createEntity(name: string, routes?: RouteMap): Entity{
+    return new Entity(
       name,
-      routes:{}
-    }
+      routes
+    )
   }
   
-  addEntity(): this {
+  addEntity(v: Entity): this {
   
+    if(this.info.miscRoutes[v.name]){
+      throw new Error(joinMessages('OreDoc already has a misc route with path:',v.name));
+    }
+  
+    this.info.entities[v.name] = v;
     return this;
   }
   
   addMiscRoute(v: RouteInfo): this {
+    
+    if(this.info.miscRoutes[v.path]){
+      throw new Error(joinMessages('OreDoc already has a misc route with path:',v.path));
+    }
   
     return this;
   }
